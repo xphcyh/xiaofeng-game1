a58b33caf930ad40d152925f9e49c1ac50fcbdec
xiaof <xiaofeng0831@hotmail.com>
2025-08-03 11:15:48 +0800

    打印python版本

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-#开头加下python标识
-
 #!/usr/bin/env python
+import sys
 
 from datetime import datetime
 
@@ -10,3 +9,5 @@ if __name__ == '__main__':
     print("今天是:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     # 输出运行结束日志
     print("运行结束")
+    # 打印python版本
+    print(sys.version)
