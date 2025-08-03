1080dfc335a0b1c6ce9f39fa22ac76be9844086a
xiaof <xiaofeng0831@hotmail.com>
2025-08-03 11:28:52 +0800

    打印电脑名称

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import sys
+import socket
 
 from datetime import datetime
 
 if __name__ == '__main__':
     print("hello world")
+    # 输出电脑名称
+    print("电脑名称:", socket.gethostname())
     # 输出今天几号，几点
     print("今天是:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     # 输出运行结束日志
