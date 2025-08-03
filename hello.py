e2c0db2a22a1adf9c8f46b1fd702a6e6999c8394
xiaof <xiaofeng0831@hotmail.com>
2025-08-03 11:31:46 +0800

    打印电脑用户名

--- hello.py
+++ hello.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 import sys
 import socket
+import os
 
 from datetime import datetime
 
 if __name__ == '__main__':
-    print("hello world")
+    print("运行开始")
+    # 打印python版本
+    print(sys.version)
+    print("hello", os.getlogin())
     # 输出电脑名称
     print("电脑名称:", socket.gethostname())
     # 输出今天几号，几点
     print("今天是:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     # 输出运行结束日志
     print("运行结束")
-    # 打印python版本
-    print(sys.version)
